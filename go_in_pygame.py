60417b70999457f1a68f9e7d9a445afce5b5ab96
mncrftfrcnm <mncrft@gmail.com>
2020-08-02 10:46:40 +1000

    upgrade my prog

--- go_in_pygame.py
+++ go_in_pygame.py
@@ -3,7 +3,40 @@ import sys
 from pygame.sprite import Sprite
 from pygame.sprite import Group
 from random import *
+patrons = 200
+def chek_fleet_duraction(ai_settings,aliens):
+    for alien in aliens.sprites():
+        alien.rect.y += ai_settings.fleet_drop_speed
+    ai_settings.fleet_direction *= -1
+    ai_settings.alien_speed_factor += 0.01
 
+def chek_fleet_edges(ai_settings,aliens):
+    for alien in aliens.sprites():
+        if alien.chek_edges():
+            chek_fleet_duraction(ai_settings,aliens)
+            break
+        
+
+bullets_die = True
+
+def get_number_rows(ai_settings,ship_height,alien_height):
+    available_spase_y = ((ai_settings.screen_height - 4*alien_height) - ship_height)
+    number_rows = int(available_spase_y / (3*alien_height))
+    return number_rows
+def create_alien(ai_settings,screen,aliens,alien_number,row_number):
+    alien = Alien(ai_settings,screen)
+
+    alien_width = alien.rect.width
+    available_spase_x = ai_settings.screen_width - 2*alien_width
+    number_aliens_x = int(available_spase_x / (2*alien_width))
+    alien.x = randint(0,300)
+    alien.rect.x = alien.x
+ #   alien_width = alien.rect.width
+    
+    alien.y = randint(0,300)
+    alien.rect.y = alien.y
+    aliens.add(alien)
+    print(alien.x,alien.y)
 def create_piano(ai_settings,ships,screen):
     for x in range(20):
         ship = Ship(ai_settings,screen)
@@ -14,14 +47,14 @@ class Settings():
         self.fleet_diraction = 1
         self.fleet_drop_speed = 10
         self.fleet_direction = 1
-        self.screen_width =  300
+        self.screen_width =  700
         self.bullet_speed_factor = 3
-        self.screen_height = 300
+        self.screen_height = 700
         self.bg_color = (20,250,200)
         self.ship_speed_factor = 1
         self.bullet_color = 60,60,60
         self.alien_speed_factor = 1
-        self.bullet_width = 644
+        self.bullet_width = 64
         self.bullet_height = 4
         self.bullet_allowed =  133
 class Bullet(Sprite):
@@ -36,7 +69,7 @@ class Bullet(Sprite):
         self.speed_factor = ai_settings.bullet_speed_factor
     def update(self):
         self.y -= self.speed_factor
-        self.rect.y = self.y
+        self.rect.y -= 0.1
 
 
         
@@ -44,14 +77,14 @@ class Bullet(Sprite):
         pygame.draw.rect(self.screen,self.color,self.rect)
     
 class Ship(Sprite):
-    def __init__(self,ai_settings,screen):
+    def __init__(self,ai_settings,screen,mouse_x,mouse_y):
         super(Ship, self).__init__()
 
         self.screen = screen
         self.color = (0,0,0)
 
         #self.image = pygame.Rect(mouse_x,mouse_y,ai_settings.bullet_width,ai_settings.bullet_height)
-        self.rect = pygame.Rect(0,0,ai_settings.bullet_width,ai_settings.bullet_height)
+        self.rect = pygame.Rect(mouse_x,mouse_y,ai_settings.bullet_width,ai_settings.bullet_height)
         self.movin_right = False
         self.movin_left = False
         self.ai_settings = ai_settings
@@ -59,8 +92,7 @@ class Ship(Sprite):
         
         
         self.screen_rect = screen.get_rect()
-        self.rect.centerx = self.screen_rect.centerx
-        self.rect.bottom = self.screen_rect.bottom
+
         self.senter = float(self.rect.centerx)
     def update(self):
         pass
@@ -69,12 +101,12 @@ class Ship(Sprite):
 class Alien(Sprite):
     def __init__(self,ai_settings,screen,ship):
         super(Alien, self).__init__()
-        self.ship =ship 
         self.color = (0,0,0)
-        self.plus_y = randint(5,200)
-        self.plus_x = randint(5,200)
         self.ai_settings = ai_settings
         self.screen = screen
+        self.xp = randint(0,100)
+        self.yp = randint(0,100)
+        
         
 
         self.image = self.rect = pygame.Rect(0,0,ai_settings.bullet_width,ai_settings.bullet_height)
@@ -82,8 +114,7 @@ class Alien(Sprite):
 
         
         
-        self.rect.x = self.rect.width
-        self.rect.y = self.rect.height
+        self.ship = ship
         self.x = float(self.rect.x)
         self.y = float(self.rect.y)
 
@@ -99,8 +130,8 @@ class Alien(Sprite):
     def update(self):
 
 
-        self.rect.x = self.ship.rect.x+plus_x
-        self.rect.y = self.ship.rect.y+plus_y
+        self.rect.x = self.ship.rect.x + self.xp
+        self.rect.y = self.ship.rect.y + self.yp
 
         
 class Blocks(Sprite):
@@ -146,30 +177,13 @@ def run_game():
     ai_settings =Settings()
     screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
     pygame.display.set_caption('Alien Invasion')
-    ship = Ship(ai_settings,screen)
+    #ship = Ship(ai_settings,screen)
     bullets = Group()
     aliens = Group()
     ships = Group()
-    ships.add(ship)
-    create_piano(ai_settings,ships,screen)
-    alien = Alien(ai_settings,screen)
-    alien_width = alien.rect.width
-    available_spase_x = ai_settings.screen_width - 2*alien_width
-    number_aliens_x = int(available_spase_x / (2*alien_width))
-    number_rows = get_number_rows(ai_settings,ship.rect.height,alien.rect.height)
-    alien.y = -90
-    alien.rect.y = alien.y
+    ship = Ship(ai_settings,screen,0,0)
+    #ships.add(ship)
     
-    for alien_number in range(number_aliens_x):
-            
-        
-        
-        #    print(alien.rect.y)
-           
-        create_alien(ai_settings,screen,aliens,alien_number,5)
-      # aliens.add(alien)
-
-
     bg_color = (20,250,200)
     while True:
         
@@ -210,14 +224,8 @@ def run_game():
                     
                     ship.movin_right = True
                 if event.key == pygame.K_SPACE:
-                    print("IOU")
-                    for alien in ships.sprites():
-                        
-                        new_bullet = Bullet(ai_settings,screen,alien,0,0)
-                        bullets.add(new_bullet)
-                    print(len(ships))
-                    print(len(bullets))
-                                                
+                    alien = Alien(ai_settings,screen,ship)
+                    aliens.add(alien)                                             
                 if event.key == pygame.K_LEFT:
                     ship.movin_left = True
             elif event.type == pygame.KEYUP:
@@ -226,13 +234,14 @@ def run_game():
                 if event.key == pygame.K_LEFT:
                     ship.movin_left = False
             elif event.type == pygame.MOUSEBUTTONDOWN:
-                
+                turtle = 0
                 mouse_x,mouse_y = pygame.mouse.get_pos()
-                for ship in ships.sprites():
-                    ship.rect.x = mouse_x
-                    ship.rect.y = mouse_y
-                    
 
+
+        #        if turtle== 1:
+                    #shi[]
+                ship.rect.x = mouse_x
+                ship.rect.y = mouse_y
         ships.update()
         
         bullets.update()
@@ -243,65 +252,19 @@ def run_game():
         screen.fill(ai_settings.bg_color)
         for bullet in bullets.sprites():
             bullet.draw_bullet()
-            first = randint(0,200)
-            second = randint(0,200)
-            three = randint(0,200)
-            ai_settings.bullet_color = (first,second,three)
-            
-            collisions = pygame.sprite.groupcollide(bullets,aliens,bullets_die,True)
-            if len(aliens) ==  0:
-                bullets.empty()
-                for row_number in range(number_rows):
-                    for alien_number in range(number_aliens_x):
-                            
-                        
-                        
-                #         #    print(alien.rect.y)
-                        
-                        create_alien(ai_settings,screen,aliens,alien_number,row_number)
-        
-        chek_fleet_edges(ai_settings,aliens)
-        aliens.update()
-        if pygame.sprite.spritecollideany(ship,aliens):
-            pass
-        pygame.display.flip()
-        for bullet in ships.sprites():
-            bullet.blitme()
-            first = randint(0,200)
-            second = randint(0,200)
-            three = randint(0,200)
-            ai_settings.bullet_color = (first,second,three)
-            
-            collisions = pygame.sprite.groupcollide(bullets,aliens,bullets_die,True)
-            if len(aliens) ==  0:
-                bullets.empty()
-                for row_number in range(number_rows):
-                    for alien_number in range(number_aliens_x):
-                            
-                        
-                        
-                #         #    print(alien.rect.y)
-                        
-                        create_alien(ai_settings,screen,aliens,alien_number,row_number)
-        pygame.display.flip()
         for bullet in aliens.sprites():
             bullet.blitme()
-            first = randint(0,200)
-            second = randint(0,200)
-            three = randint(0,200)
-            ai_settings.bullet_color = (first,second,three)
-            
-            collisions = pygame.sprite.groupcollide(bullets,aliens,bullets_die,True)
-            if len(aliens) ==  0:
-                bullets.empty()
-                for row_number in range(number_rows):
-                    for alien_number in range(number_aliens_x):
-                            
-                        
-                        
-                #         #    print(alien.rect.y)
-                        
-                        create_alien(ai_settings,screen,aliens,alien_number,row_number)
+
+        chek_fleet_edges(ai_settings,aliens)
+        aliens.update()
+        # for bullet in ships.sprites():
+        #     bullet.blitme()
+        #     first = randint(0,200)
+        #     second = randint(0,200)
+        #     three = randint(0,200)
+        #     ai_settings.bullet_color = (first,second,three)
+        ship.blitme()
         pygame.display.flip()
+
 while True:
     run_game()
